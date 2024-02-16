5a43b7d0f40026f700b13c6da704f3e6c06504eb
Prajakta <pawarprajakta1610@gmail.com>
2024-02-16 10:16:19 -0500

    initial commit

---
+++ src/main/java/com/psp/demo/controller/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.psp.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+	
+	
+	@GetMapping("api/v1/hello")
+	public ResponseEntity<String> ping() {
+		return new ResponseEntity<String>("Hello World from My first Project", HttpStatus.OK);
+		
+		
+	}
+
+}
